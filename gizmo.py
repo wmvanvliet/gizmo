07cffa997b523669dd9aeb18ae2ba7ea3c4b7a24
susamerz <58432750+susamerz@users.noreply.github.com>
2020-07-07 07:54:58 +0300

    Implemented initial idea for methods asked

--- gizmo.py
+++ gizmo.py
@@ -1,1 +1,15 @@
 # file for python testing test
+class Gizmo
+
+  def _init_(self, name):
+    self.name = name
+  
+  def speak():
+    print(self.name)
+  
+  @property
+  def friendship_name(self):
+    """ The friendship name, i.e. the name, reversed and capitalized as a proper noun """
+    inv_name=self.name[::-1]
+    print(inv_name.casefold().capitalize())
+  
