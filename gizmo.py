ef637f66b49bb9497202ec1ca58eab63854b553e
Susa Merz <susa.merz@gmail.com>
2020-07-15 11:02:24 +0300

    added implementation of ex6

--- gizmo.py
+++ gizmo.py
@@ -12,4 +12,11 @@ class Gizmo:
     """ The friendship name, i.e. the name, reversed and capitalized as a proper noun """
     inv_name=self.name[::-1]
     return inv_name.casefold().capitalize()
-  
+
+
+  def spell(self):
+    outstring= self.name[0]
+    for elem in self.name[1:len(self.name)+1]:
+      outstring = outstring+'.'+elem
+    print(outstring)
+
