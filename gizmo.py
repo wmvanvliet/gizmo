34e8a20e08c9d61c38a915619e4f369415e2b732
Marijn van Vliet <w.m.vanvliet@gmail.com>
2020-07-16 08:31:47 +0300

    Do exercises #10 and #11

--- gizmo.py
+++ gizmo.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Gizmo:
     def __init__(self, name):
         self.name = name
@@ -8,3 +10,9 @@ class Gizmo:
     @property
     def friendship_name(self):
         return self.name[::-1].title()
+
+def multiplication_table(zero_out_multiples=None):
+    table = np.outer(np.arange(1, 13), np.arange(1, 13))
+    if zero_out_multiples is not None:
+        table[table % zero_out_multiples == 0] = 0
+    return table
