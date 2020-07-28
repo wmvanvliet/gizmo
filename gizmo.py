d4b0620620ca4bc0d748b5b024bb25e5570ce02e
Philip Chimento <philip.chimento@gmail.com>
2020-07-27 20:21:55 -0700

    ex4

--- gizmo.py
+++ gizmo.py
@@ -1,6 +1,14 @@
+import os.path
+
+
 def hello(name, country='Finland'):
     print(f'Hello {name}, how are things in {country}?')
 
 
 def spell(word):
     print('.'.join(list(word)))
+
+
+def relative_path(ids):
+    return [os.path.join('.', 'subjects', f'mock_recording_{i}.rec')
+            for i in ids]
