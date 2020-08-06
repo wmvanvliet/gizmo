5a98145a9fd45230c21ef4e673ee773b20f7c296
klugeannika <klugeannika@gmail.com>
2020-08-06 18:48:18 +0300

    Update gizmo.py

--- gizmo.py
+++ gizmo.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Exercise 2
 def hello(name, country='Finland'):
   print(f'Hello {name}, how are things in {country}?')
@@ -8,7 +10,7 @@ def spell(word):
     for i in range(len(word)-1):
         ans += word[i]
         ans += '.'
-    ans += word[len(word)]
+    ans += word[len(word)-1]
     print(ans)
     
 # Exercise 4    
@@ -32,9 +34,7 @@ class Gizmo:
 
 # Exercises 8, 9 and 10
 
-import numpy as np
-
-def multiplication_table(zero_out_multiples=None):
+def multiplication_table(zero_out_multiples = None):
     """Multiplication table from 1 to 12.
 
 This function returns the multiplication table from 1 to 12 as a 2-dimensional array.
@@ -57,24 +57,35 @@ out : ndarray
     out = np.outer(a, b)
     if zero_out_multiples:
         out[out % zero_out_multiples == 0] = 0
-    
     return out
-       
-    
+
+# Exercise 11
 def generate_fibonacci_sequence(n):
-  n1, n2 = 0, 1
-  cnt = 0
-  if n==0:
-    return
-  elif n==1:
-    return(n1)
-  elif n>1:
+    n1, n2 = 0, 1
+    cnt = 0
+    if n==0:
+        yield()  
+    elif n==1:
+        yield(n1)
+    elif n>1:
+        while cnt < n:
+            yield(n1)
+            nth = n1 + n2
+            yield(nth)
+            # update values
+            n1 = n2
+            n2 = nth
+            cnt += 2
+
+# Exercise 12
+def get_fibonacci_sequence(n):
+    seq = []
+    cnt = 0
+    fb = generate_fibonacci_sequence(n)
     while cnt < n:
-      yield(n1)
-      nth = n1 + n2
-      yield(nth)
-      # update values
-      n1 = n2
-      n2 = nth
-      cnt += 2
+        seq.append(next(fb))
+        cnt += 1
+    return seq
+
+
     
