a0e4962cb9ab7ac3774b01539f86adc15f827ff0
klugeannika <klugeannika@gmail.com>
2020-08-06 17:19:39 +0300

    Update gizmo.py

--- gizmo.py
+++ gizmo.py
@@ -1,4 +1,10 @@
 def hello(name, country='Finland'):
   print('Hello ', name, ', how are things in ', country, '?', sep='')
 
-
+def spell(word):
+    ans = ""
+    for i in range(len(word)):
+        ans += word[i]
+        ans += '.'
+    print(ans)
+    
