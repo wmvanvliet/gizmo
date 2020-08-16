78d3497ac6536c2241ffced4a14040c5f6108c01
user <user@users-MBP-2.domain_not_set.invalid>
2020-08-16 23:24:45 +0200

    Exercise 10: Document your function using numpydoc

--- gizmo.py
+++ gizmo.py
@@ -25,6 +25,20 @@ def relative_path(subject_identifiers):
 
 
 def multiplication_table(zero_out_multiples=None):
+    """Returns a multiplication table from 1 to 12
+
+    The table shows you the results of multiplying two numbers. One number is along a row, the other down a column, and the results are shown where a row and column meet.
+
+    Parameters
+    ----------
+    zero_out_multiples : int, optional
+        When this parameter is set to an integer number, then the multiplication table that is returned by the function will have all multiples of the given number set to zero.
+
+    Returns
+    -------
+    multiplication_table : ndarray
+        Multiplication table
+    """
     array = np.arange(1, 13)
     if zero_out_multiples:
         array[array % zero_out_multiples == 0] = 0
