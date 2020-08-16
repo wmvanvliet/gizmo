17af56ca07c605f1b4aaa467cd4b872cd27e2144
user <user@users-MBP-2.domain_not_set.invalid>
2020-08-16 22:35:19 +0200

    Exercise 4: Use string formatting

--- gizmo.py
+++ gizmo.py
@@ -4,3 +4,10 @@ def hello(name, country="Finland"):
 
 def spell(word):
     print(".".join(word))
+
+
+def relative_path(subject_identifiers):
+    return [
+        f"./subjects/mock_recording_{subject_identifier}.rec"
+        for subject_identifier in subject_identifiers
+    ]
