b439c386d94a91bee295b3a580248e2c595cd611
gghazar <gghazar@github.com>
2020-08-27 11:59:48 +0300

    exercise 3

--- gizmo.py
+++ gizmo.py
@@ -1,3 +1,13 @@
 # exercise 2
 def hello(name, country="Finland"):
     print(f"Hello {name}, how are things in {country}?")
+
+# exercise 3
+
+def spell(word):
+    spelled = ""
+    for i in word:
+        spelled += i + "."
+    print(spelled[:-1])
+        
+        
