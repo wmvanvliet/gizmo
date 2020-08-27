5f1ab40ed35e77f848f769c7cb10d7f81b389176
Eurus-J-Zhang <66300568+Eurus-J-Zhang@users.noreply.github.com>
2020-08-27 15:24:27 +0300

    Update gizmo.py

--- gizmo.py
+++ gizmo.py
@@ -3,3 +3,10 @@ import gizmo
   
 def hello(a,country='Finland'):
   print('Hello %s, how are things in %s?'%(a, country) )
+
+def spell(word):
+  spell_word=''
+  for i in word:
+    spell_word=spell_word+i+'.'
+    spell_word = spell_word[:-1]
+  print(spell_word)
