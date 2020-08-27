f9a9d32ee5e7a708a67e2add3e9ffde8f4e1ad22
Eurus-J-Zhang <66300568+Eurus-J-Zhang@users.noreply.github.com>
2020-08-27 15:08:42 +0300

    Update gizmo.py

--- gizmo.py
+++ gizmo.py
@@ -1,4 +1,5 @@
 import gizmo
-def gizmo.hello(a,country='Finland'):
-  print('Hello',a, 'how are things in', contry,'?' )
+
   
+def gizmo.hello(a,country='Finland'):
+  print('Hello %s, how are things in %s?'%(a, country) )
