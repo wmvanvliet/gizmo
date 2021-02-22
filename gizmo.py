ea5c33345dd405fad728e020de051f0fa7379ca8
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2021-02-22 15:17:03 +0200

    Add spell function

--- gizmo.py
+++ gizmo.py
@@ -1,2 +1,11 @@
 def hello(name, country='Finland'):
     print(f'Hello {name}, how are things in {country}?')
+
+
+def spell(word):
+    if len(word) == 0:
+        return
+    s = word[0]
+    for letter in word[1:]:
+        s += '.' + letter
+    print(s)
