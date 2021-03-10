3fea97182351fffa737a0863077412215db79022
bluesaturn <bestenborstel@gmail.com>
2021-03-10 10:30:59 +0100

    Current version includes exercises 2,3,4,5

--- gizmo.py
+++ gizmo.py
@@ -1,1 +1,21 @@
-import gizmo
+def hello(name,country="Finland"):
+    print("Hello {0}, how are things in {1}?".format(str(name),str(country)))
+
+def spell(input_str):
+    result=input_str[0]
+    for i in input_str[1:]:
+        result+="."+i
+        
+    return print(result)
+
+def relative_path(subject_identifiers,relpath='./subjects/moc_recording'):
+    names=[]
+    relpath='./subjects/mock_recording_'
+    for i in subject_identifiers:
+        res=relpath+"_"+i+".rec"
+        names.append(res)
+
+    return names
+
+class Gizmo:
+    pass
