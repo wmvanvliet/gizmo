0381728f3f2f658109055c51e7fd79427185e42b
Niket <niketagrawal8@gmail.com>
2021-03-27 21:11:06 +0100

    exercise 9, 10 completed

--- gizmo.py
+++ gizmo.py
@@ -20,7 +20,8 @@ def spell(word):
 def relative_path(list):
     output_list = []
     for subject_identifier in list:
-        output_list.append(f'./subjects/mock_recording_{subject_identifier}.rec')
+        output_list.append(
+            f'./subjects/mock_recording_{subject_identifier}.rec')
     return output_list
 
 class Gizmo:
@@ -33,7 +34,31 @@ class Gizmo:
     def speak(self):
         print(self.name)
 
-def multiplication_table():
+def multiplication_table(zero_out_multiples=None):
+    """Creates a multiplication table.
+
+    Returns a two-dimensional NumPy array that contains the multiplication 
+    table from 1 to 12.  
+    
+    Paramateres
+    -----------
+    zero_out_multiples : int
+        When this parameter is set to an integer number, then the 
+        multiplication table that is returned by the function will have all
+        multiples of the given number set to zero. The default value of this 
+        parameter is None.
+
+    Returns
+    -------
+    array : numpy ndarray
+        A two-dimensional NumPy array that contains the multiplication table 
+        from 1 to 12 with the multiples of zero_out_multiples paramaters 
+        passed as argument set to zero. 
+    """
     a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    return (np.outer(a, a))
+    array = np.outer(a, a)
+    array[array % zero_out_multiples == 0] = 0
+    return array
+    
+
 
