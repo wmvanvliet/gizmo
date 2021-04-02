4cf0e5e49ea720423d11fea738cb3274663d29fb
Matilda Makkonen <matilda.makkonen@aalto.fi>
2021-04-02 13:01:34 +0300

    Exercise 4: added function spell

--- gizmo.py
+++ gizmo.py
@@ -1,2 +1,8 @@
 def hello(name, country):
     print("Hello %s, how are things in %s?" % (name, country))
+    
+def spell(word):
+    for i in word:
+        print(i,end='')
+        if i != word[-1]:
+            print(".",end='')
