720a21de0150deb57655d6425e6c30a9e1355e5e
Laurinoja Joonas <joonas.laurinoja@aalto.fi>
2021-04-02 13:01:39 +0300

    added file spell()

--- gizmo.py
+++ gizmo.py
@@ -2,3 +2,14 @@ def hello(name, country):
     
     print("Hello %s, how are things in %s?" % (name, country))
     
+def spell(word):
+    
+    for i in word:
+        print(i, end = '')
+        if i != word[-1]:
+            print(".", end = '')
+            
+
+    
+    
+spell("Joonas")
