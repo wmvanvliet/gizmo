17fdb96463b6d6c087c9bb18909a633972feffbf
Tavast Mikke <tavastm1@vdiubuntu095.org.aalto.fi>
2021-04-04 15:52:52 +0300

    corrections to exc 8,9

--- gizmo.py
+++ gizmo.py
@@ -5,7 +5,7 @@ Created on Fri Apr  2 16:17:18 2021
 
 @author: tavastm1
 """
-import gizmo
+
 import numpy as np
 import pandas as pd
 
@@ -39,7 +39,7 @@ class Gizmo:
     def speak(self):
         return print(self.name)
     
-def multiplication_table(a=list(range(1,13)),b=list(range(1,13)), zero_out_multiplies=None):
+def multiplication_table(a=1,b=12, zero_out_multiplies=None):
     """
     Creates a multiplication table from a to b. 
     
@@ -55,7 +55,9 @@ def multiplication_table(a=list(range(1,13)),b=list(range(1,13)), zero_out_multi
         A multiplication table from a to b. 
     """ 
     temp = np.outer(list(range(a,b+1)),list(range(a,b+1)))
-    temp[temp % zero_out_multiplies == 0] = 0   
+    if zero_out_multiplies is not None:
+        temp[temp % zero_out_multiplies == 0] = 0   
+        
     return temp
 
 def get_titanic():
