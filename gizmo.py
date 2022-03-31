559feff3fab61e73e63de295a2fe2a175a0ccb35
Thomas Pfau <github@thomaspfau.de>
2022-03-31 09:39:14 +0300

    Move fibonacci function. I should read exercises in more detail...

--- gizmo.py
+++ gizmo.py
@@ -18,6 +18,37 @@ def spell(word):
 def relative_path(subjects, basename='./subjects/mock_recording_', extension='.rec' ):
     return [(basename + '{subj}' + extension).format(subj=subj) for subj in subjects]
 
+
+def generate_fibonacci_sequence(self,maxcount):
+    """ This function returns a fibonacci number generator.
+    
+    generate_fibonacci_sequence(maxcount)
+        
+    The returned generator yields fibonacci numbers up to the maximum number indicated.
+    Note, that at least 2 numbers are always yielded!
+        
+    Parameters
+    ----------
+    maxcount: int
+        The number of fibonacci numbers to be yielded by the generator.
+        
+    Yields
+    -------        
+    fibonacci: int
+        The n-th number yielded is the n-th fibonacci number  
+     
+    """
+    last = 0
+    yield last
+    current = 1
+    yield current
+    for i in range(maxcount-2):
+        next = current+last
+        last = current
+        current = next
+        yield next    
+
+
 class Gizmo:
     
     def __init__(self, name):
@@ -26,32 +57,4 @@ class Gizmo:
     def speak(self):
         print(self.name)
     
-    def generate_fibonacci_sequence(self,maxcount):
-        """ This function returns a fibonacci number generator.
-        
-        generate_fibonacci_sequence(maxcount)
-        
-        The returned generator yields fibonacci numbers up to the maximum number indicated.
-        Note, that at least 2 numbers are always yielded!
-        
-        Parameters
-        ----------
-        maxcount: int
-            The number of fibonacci numbers to be yielded by the generator.
-        
-        Yields
-        -------        
-        fibonacci: int
-            The n-th number yielded is the n-th fibonacci number  
-         
-        """
-        last = 0
-        yield last
-        current = 1
-        yield current
-        for i in range(maxcount-2):
-            next = current+last
-            last = current
-            current = next
-            yield next    
     
