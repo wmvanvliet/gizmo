93a973f8065bbe18eb732d9c328e8df47bb7ae48
Hasti Narimanzadeh <hasti.nz@gmail.com>
2022-04-15 00:38:01 +0300

    Add spell function and add return value for hello

--- gizmo.py
+++ gizmo.py
@@ -1,3 +1,11 @@
 def hello(name, country="Finland"):
-    print(f"Hello {name}, how are things in {country}")
+    return f"Hello {name}, how are things in {country}"
+
+def spell(word):
+    spelling = ''
+    for l in word:
+        spelling += l
+        spelling += '.'
+    return spelling[:-1]
+
 
