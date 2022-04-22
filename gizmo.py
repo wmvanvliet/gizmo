283173848e71d2297778eb10a7de2380eb5b899f
Kunal Ghosh <kunalghosh@users.noreply.github.com>
2022-04-22 13:55:13 +0300

    Update gizmo.py

--- gizmo.py
+++ gizmo.py
@@ -1,1 +1,10 @@
 # Test gizmo file
+def hello(name, country='Finland'):
+  print('Hello {name}, how are things in {country}?')
+  
+def spell(name):
+  total_chars = len(name)
+  for idx,char in enumerate(name):
+    print(char, end="")
+    if idx != total_chars-1:
+      print(".", end="")
