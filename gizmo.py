e37a4cb370ee4999fe83ea9009cca61995d9acbd
joan-pijpker <73435463+joan-pijpker@users.noreply.github.com>
2023-06-26 15:42:39 +0200

    Update gizmo.py

--- gizmo.py
+++ gizmo.py
@@ -1,2 +1,11 @@
 def hello(name, country = "Finland"):
     print("Hello ", name, " How's life in ", country, "?")
+
+def spell(woord):
+    teller = 1
+    for i in woord:
+        print(i, end='')
+        if teller < len(woord):
+            print(".", end='')
+        teller += 1
+    print()
