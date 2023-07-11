e93768d5798cccf14b0ce296cce3149eb8fd44c8
Livia <136809371+Liviavanvliet@users.noreply.github.com>
2023-07-11 14:26:30 +0200

    exercise 3
    added some string functions

--- gizmo.py
+++ gizmo.py
@@ -1,1 +1,15 @@
+# Function for saying hello, with the default country set to Finland
+def hello(name, country = "Finland"):
+    print("Hello", name + ", how are things in", country + "?")
 
+# Spelling function
+def spell(word):
+    for letter in word:
+        print (letter + '.', end = '')
+
+# creating a relative path finder
+# takes an array as an argument and parses it into strings
+def relative_path(arr):
+    for i in arr:
+        i = './subjects/mock_recording_'+i+'.rec'
+        print(i)
