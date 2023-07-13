37230e7dad3026fbfd2a52ae41f11154762fe267
Livia van Vliet <lsteernstra@tudelft.nl>
2023-07-13 15:16:09 +0200

    added spelling fuction

--- gizmo.py
+++ gizmo.py
@@ -1,3 +1,31 @@
 # hello function
 def hello(name, country = "Finland"):
     print("Hello", name + ", how are things in", country + "?")
+# spelling function
+def spell(word):
+    for letter in word:
+        print (letter + '.', end = '')
+        
+# editing strings
+def relative_path(arr):
+    for i in arr:
+        i = './subjects/mock_recording_'+i+'.rec'
+        print(i)
+
+# creating a class     
+class Gizmo:
+    def __init__(self,name = "default"):
+        self.name = name
+        
+    def speak(self):
+        print(self.name)
+    
+# function for fibonacci
+def generate_fibonacci_sequence(n):
+    seq = []
+    iterator = iter(seq)
+    a, b = 0,1
+    while len(seq) < n:
+        seq.append(a)
+        a, b = b, a + b
+    return iterator
